0dfa58e430547d2bba94dc9a434ccdad84fcaa01
sakula <sakula@sakula6420.VENTERA.com>
2015-06-18 16:50:51 -0400

    #1 additional autocomplete commits

--- 18FDemo/web/app/components/drugs/drugscontroller.js
+++ 18FDemo/web/app/components/drugs/drugscontroller.js
@@ -1,14 +1,11 @@
 var drugscontrollers = {};
 
 drugscontrollers.DrugsController = function($scope, $http, $location, $routeParams) {
-    
-    $scope.drugnames = [];
-    $scope.suggestDrugs = function() {
-	$scope.term = $routeParams.data;
-	$http.get('https://api.fda.gov/drug/label.json?limit=10&search=openfda.brand_name:' + $scope.term + '+openfda.generic_name:' + $scope.term).success(
-		function(jsonResp) {
-		    $scope.rows = jsonResp;
-		});
+    $scope.term = '';
+    $scope.selecteddrugs = [];
+    $scope.move = function() {
+	//alert('Hi' + $scope.term);
+	$scope.selecteddrugs.push({value: $scope.term, text:$scope.term});
     }
 }
 
