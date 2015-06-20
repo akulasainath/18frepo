2e5fcaf136d7ec2d5d320b361e6f8aa801cae694
sakula <sakula@sakula6420.VENTERA.com>
2015-06-20 19:30:54 -0400

    #4 updated the logic to view top reactions based on drug entered by user.

--- 18FDemo/web/app/components/drugs/drugscontroller.js
+++ 18FDemo/web/app/components/drugs/drugscontroller.js
@@ -1,88 +1,94 @@
 var drugscontrollers = {};
 
-drugscontrollers.DrugsController = function($scope, $http, $location, $routeParams) {
-    $scope.termBrand = '';
+drugscontrollers.DrugsController = function($scope, $http, $location,
+	$routeParams) {
     $scope.termGeneric = '';
+    $scope.country = '';
     $scope.selecteddrugs = [];
-    $scope.drugreactions = [];
-    $scope.serious = [];
+    $scope.headerItems = [];
+    $scope.storage = [];
+    $scope.finalstorage = [];
+    $scope.searchterms = [];
     
-    $scope.move = function(type) {
-	var flag = true;
-	$($scope.selecteddrugs).each(function(){
-	    if($(this)[0].term == $scope.termGeneric || $(this)[0].term == $scope.termBrand) {
-		flag = false;;
-	    }
+    $scope.move = function() {
+	$("#progressbar").show();
+	$scope.selecteddrugs.push($scope.termGeneric);
+	$scope.indiSearch($scope.termGeneric);
+	$scope.combinedSearch();
+    }
+    
+    /**
+     * Method for Individual Drug Search.
+     * For Ex: Where drug brand name or generic name is Drug A
+     */
+    $scope.indiSearch = function(term) {
+	var searchTerm = '"'+term.replace(/\,/g, '').replace(/\ /g, '+')+'"';
+	$scope.searchterms.push(searchTerm);
+	$http.get('https://api.fda.gov/drug/event.json?count=patient.reaction.reactionmeddrapt.exact&search=(patient.drug.openfda.generic_name:' + searchTerm + "+patient.drug.openfda.brand_name:" + searchTerm + ")")
+        	.success(function(jsonResp) {
+        	    $scope.storage[searchTerm] = jsonResp.results;
+        	}).error(function(errorResp) {
+        	    $scope.storage[searchTerm] = new Object();
+		});
+    }
+
+    /**
+     * Method for Combined Drug Search.
+     * For Ex: Where drug brand name or generic name is Drug A + Drug B + Drug C
+     */
+    $scope.combinedSearch = function() {
+	var combinedSearchTerm = $scope.searchterms.join('+');
+	$scope.finalstorage = [];
+	$http.get('https://api.fda.gov/drug/event.json?count=patient.reaction.reactionmeddrapt.exact&search=(patient.drug.openfda.generic_name:' + combinedSearchTerm + "+patient.drug.openfda.brand_name:" + combinedSearchTerm + ")")
+	.success(function(jsonResp) {
+	    $scope.headerItems = [];
+	    $(jsonResp.results).each(function(index) {
+		if(index < 10) {
+		    $scope.headerItems.push($(this)[0]);
+		}
+	    });
+	    //loop thru each search term to get 5 counts
+	    $($scope.searchterms).each(function(index){
+		var currentTerm = $scope.searchterms[index];
+		var finalItems = [];
+		//loop thru header items to get the 5 reaction counts
+		$($scope.headerItems).each(function(headerIdx){
+		   var currentHeader =  $(this)[0].term;
+		   finalItems[headerIdx] = "";
+		   //loop thru all reactions for particular drug and bring out required 5 counts.
+		   $($scope.storage[currentTerm]).each(function(){
+		       if($(this)[0].term == currentHeader) {
+			    finalItems[headerIdx] = $(this)[0].count;
+		       }
+		   });
+		});
+		$scope.finalstorage[index] = finalItems;
+	    });
+	    $scope.termGeneric = '';
+	    $("#progressbar").hide();
+	}).error(function(errorResp) {
+	    alert("Error: " + errorResp.error.message);
+	    $("#progressbar").hide();
 	});
-	if(flag) {
-	    if(type == 'generic') {
-		var searchTerm = $scope.termGeneric.replace(/\,/g, '').replace(/\ /g, '+');
-		$scope.goGeneric(searchTerm);
-	    } else {
-		var searchTerm = $scope.termBrand.replace(/\,/g, '').replace(/\ /g, '+');
-		$scope.goBrand(searchTerm);
-	    }
+    }
+    
+    $scope.remove = function(term) {
+	$("#progressbar").show();
+	var searchTerm = term.replace(/\,/g, '');
+	searchTerm = '"'+term+'"';
+	var index = $scope.searchterms.indexOf(searchTerm);
+	$scope.searchterms.splice(index, 1);
+	
+	index = $scope.selecteddrugs.indexOf(term);
+	$scope.selecteddrugs.splice(index, 1);
+
+	if($scope.selecteddrugs.length > 0) {
+	    $scope.combinedSearch();
+	} else {
+	    $("#progressbar").hide();
 	}
     }
     
-    $scope.goGeneric = function(searchTerm) {
-	var tempItems = [];
-	var selectedDrugCount = -1;
-	$http.get('https://api.fda.gov/drug/event.json?count=patient.reaction.reactionmeddrapt.exact&search=patient.drug.openfda.generic_name:'+ searchTerm +'+AND+serious:1').success(function(jsonResp) {
-	    $(jsonResp.results).each(function(){
-		selectedDrugCount += $(this)[0].count;
-	    });
-	    $($scope.serious).each(function(index){
-		tempItems[index] = new Object();
-		var current = $(this)[0];
-		$(jsonResp.results).each(function(){
-		    if($(this)[0].term == current.term) {
-			tempItems[index] = $(this)[0];
-		    }
-		});
-	    });
-	    $scope.drugreactions.push(tempItems);
-	    
-	    var selectedDrug = {
-		term: $scope.termGeneric,
-		count: selectedDrugCount
-	    };
-	    $scope.selecteddrugs.push(selectedDrug);
-	    $scope.termGeneric = '';
-	}).error(function(errorResp){
-	    alert("Error: " + errorResp.error.message);
-	});
-    }
-    
-    $scope.goBrand = function(searchTerm) {
-	var tempItems = [];
-	var selectedDrugCount = -1;
-	$http.get('https://api.fda.gov/drug/event.json?count=patient.reaction.reactionmeddrapt.exact&search=patient.drug.openfda.brand_name:'+ searchTerm + '+AND+serious:1').success(function(jsonResp) {
-	    $(jsonResp.results).each(function(){
-		selectedDrugCount += $(this)[0].count;
-	    });
-	    $($scope.serious).each(function(index){
-		tempItems[index] = new Object();
-		var current = $(this)[0];
-		$(jsonResp.results).each(function(){
-		    if($(this)[0].term == current.term) {
-			tempItems[index] = $(this)[0];
-		    }
-		});
-	    });
-	    $scope.drugreactions.push(tempItems);
-	    
-	    var selectedDrug = {
-		term: $scope.termBrand,
-		count: selectedDrugCount
-	    };
-	    $scope.selecteddrugs.push(selectedDrug);
-	    $scope.termBrand = '';
-	}).error(function(errorResp){
-	    alert("Error: " + errorResp.error.message);
-	});
-    }
-
     $scope.isEmpty = function(arr) {
 	for ( var i in arr) {
 	    if (arr.hasOwnProperty(i))
@@ -100,22 +106,6 @@ drugscontrollers.DrugsController = function($scope, $http, $location, $routePara
 	}
 	return idx;
     };
-
-    $scope.remove = function(term) {
-	var index = $scope.selecteddrugs.indexOf(term);
-	$scope.selecteddrugs.splice(index, 1);
-	$scope.drugreactions.splice(index, 1);
-    }
-
-    $scope.init = function() {
-	var limit = 10;
-	$http.get("https://api.fda.gov/drug/event.json?count=patient.reaction.reactionmeddrapt.exact&search=serious:1")
-		.success(function(jsonResp) {
-		    for (i = 0; i < limit; i++) {
-			$scope.serious.push(jsonResp.results[i]);
-		    }
-		});
-    }
 }
 
 demoApp.controller(drugscontrollers);
