aa130d037b01359edf17870e469d4b84c7006533
sakula <sakula@sakula6420.VENTERA.com>
2015-06-22 22:43:24 -0400

    #27 fixed the invalid zero's display.

--- 18FDemo/web/app/components/drugs/drugscontroller.js
+++ 18FDemo/web/app/components/drugs/drugscontroller.js
@@ -1,7 +1,7 @@
-var drugscontrollers = {};
+'use strict';
 
-drugscontrollers.DrugsController = function($scope, $http, $location, $routeParams) 
-{
+angular.module('openFDAApp').controller('DrugsController', ['$scope', '$http', '$location',
+    function($scope, $http, $location) {
         $scope.termGeneric = '';
         $scope.country = '';
         $scope.sex = '';
@@ -39,12 +39,10 @@ drugscontrollers.DrugsController = function($scope, $http, $location, $routePara
         $scope.indiSearch = function(term) {
             var searchTerm = '"'+term.replace(/\,/g, '').replace(/\ /g, '+')+'"';
             $scope.searchterms.push(searchTerm);
-            $http.get('https://api.fda.gov/drug/event.json?count=patient.reaction.reactionmeddrapt.exact&limit=1000&search=(patient.drug.openfda.generic_name:' + searchTerm + '+patient.drug.openfda.brand_name:' + searchTerm + ')'+filter)
+            $http.get('https://api.fda.gov/drug/event.json?count=patient.reaction.reactionmeddrapt.exact&limit=1000&search=(patient.drug.openfda.generic_name:' + searchTerm + '+patient.drug.openfda.brand_name:' + searchTerm + ')'+filter, { cache: true})
                     .success(function(jsonResp) {
                         $scope.storage[searchTerm] = jsonResp.results;
-                    }).error(function(errorResp) {
-                        $scope.storage[searchTerm] = {};
-                });
+                    });
         };
 
         /**
@@ -53,31 +51,31 @@ drugscontrollers.DrugsController = function($scope, $http, $location, $routePara
          */
         $scope.combinedSearch = function() {
             var combinedSearchTerm = $scope.searchterms.join('+');
-            $scope.finalstorage = [];
-            $http.get('https://api.fda.gov/drug/event.json?limit=10&count=patient.reaction.reactionmeddrapt.exact&search=(patient.drug.openfda.generic_name:' + combinedSearchTerm + '+patient.drug.openfda.brand_name:' + combinedSearchTerm + ')'+filter)
+            $http.get('https://api.fda.gov/drug/event.json?limit=10&count=patient.reaction.reactionmeddrapt.exact&search=(patient.drug.openfda.generic_name:' + combinedSearchTerm + '+patient.drug.openfda.brand_name:' + combinedSearchTerm + ')'+filter, { cache: true})
             .success(function(jsonResp) {
+        	$scope.finalstorage = [];
                 $scope.headerItems = [];
                 $(jsonResp.results).each(function(index) {
-                if(index < 10) {
-                    $scope.headerItems.push($(this)[0]);
-                }
+                    if(index < 10) {
+                        $scope.headerItems.push($(this)[0]);
+                    }
                 });
                 //loop thru each search term to get 5 counts
                 $($scope.searchterms).each(function(index){
-                var currentTerm = $scope.searchterms[index];
-                var finalItems = [];
-                //loop thru header items to get the 5 reaction counts
-                $($scope.headerItems).each(function(headerIdx) {
-                   var currentHeader =  $(this)[0].term;
-                   finalItems[headerIdx] = '';
-                   //loop thru all reactions for particular drug and bring out required 5 counts.
-                   $($scope.storage[currentTerm]).each(function(){
-                       if($(this)[0].term === currentHeader) {
-                        finalItems[headerIdx] = $(this)[0].count;
-                       }
-                   });
-                });
-                $scope.finalstorage[index] = finalItems;
+                    var currentTerm = $scope.searchterms[index];
+                    var finalItems = [];
+                    //loop thru header items to get the 5 reaction counts
+                    $($scope.headerItems).each(function(headerIdx) {
+                       var currentHeader =  $(this)[0].term;
+                       finalItems[headerIdx] = '';
+                       //loop thru all reactions for particular drug and bring out required 5 counts.
+                       $($scope.storage[currentTerm]).each(function(){
+                           if($(this)[0].term === currentHeader) {
+                            finalItems[headerIdx] = $(this)[0].count;
+                           }
+                       });
+                    });
+                    $scope.finalstorage[index] = finalItems;
                 });
                 $scope.termGeneric = '';
                 $('#progressbar').hide();
@@ -123,8 +121,8 @@ drugscontrollers.DrugsController = function($scope, $http, $location, $routePara
         };
         
         $scope.applyFilter = function() {
-        	if($scope.selecteddrugs.length > 0) {
-        		$('#progressbar').show();
+            if($scope.selecteddrugs.length > 0) {
+        	$('#progressbar').show();
                 //set filters
                 $scope.setFilters();
 	        	$scope.searchterms = [];
@@ -137,5 +135,4 @@ drugscontrollers.DrugsController = function($scope, $http, $location, $routePara
 	        }
         };
     }
-
-demoApp.controller(drugscontrollers);
+]);
