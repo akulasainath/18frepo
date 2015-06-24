32eac640d95c6077f14cc4b85a32f7e0f3de7051
sakula <sakula@sakula6420.VENTERA.com>
2015-06-24 13:11:18 -0400

    #30 integrated home page.

--- 18FDemo/web/app/app.routes.js
+++ 18FDemo/web/app/app.routes.js
@@ -1,7 +1,9 @@
 'use strict';
 
 openFDAApp.config(function($routeProvider) {
-	$routeProvider.when('/drugs/', {
+	$routeProvider.when('/', {
+		templateUrl : '/demo/home.html'
+	}).when('/drugs/', {
 		templateUrl : '/demo/app/components/drugs/drugs.html',
 		controller : 'DrugsController'
 	});
