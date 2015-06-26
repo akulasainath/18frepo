14dcabf71768203644ff0d8f127f6a2d70a1555e
PPradhan <ppradhan@ventera.com>
2015-06-26 10:07:34 -0400

    #24 Identify unit testing technologies including required setup and configuration steps.
    Committing changes to filters test.

--- 18FDemo/web/test/karma/app.filters.test.js
+++ 18FDemo/web/test/karma/app.filters.test.js
@@ -1,28 +1,28 @@
-describe('Filter Test', function () {
-  'use strict'; 
-  var $filter;
-  beforeEach(function () {
-    module('openFDAApp');
-    inject(function (_$filter_) {
-      $filter = _$filter_;
-    });
-  });
+describe('Filter Test', function() {
+	'use strict';
+	var $filter;
+	beforeEach(function() {
+		module('openFDAApp');
+		inject(function(_$filter_) {
+			$filter = _$filter_;
+		});
+	});
 
-  it('string', function () {
-    var foo = 'Test String', result;
-    result = $filter('textOrNumber')(foo, 'string');
-    expect(result).toEqual('<b>-</b>');
-  });
-  
-  it('special char', function () {
-	    var foo = '#', result;
-	    result = $filter('textOrNumber')(foo, 'special char');
-	    expect(result).toEqual('<b>-</b>');
-	  });
-  
-  it('number', function () {
-	    var foo = 12, result;
-	    result = $filter('textOrNumber')(foo, Number(foo));
-	    expect(result).toEqual('12');
-	  });
+	it('string', function() {
+		var foo = 'Test String', result;
+		result = $filter('textOrNumber')(foo, 'string');
+		expect(result).toEqual('<b>-</b>');
+	});
+
+	it('special char', function() {
+		var foo = '#', result;
+		result = $filter('textOrNumber')(foo, 'special char');
+		expect(result).toEqual('<b>-</b>');
+	});
+
+	it('number', function() {
+		var foo = 12, result;
+		result = $filter('textOrNumber')(foo, Number(foo));
+		expect(result).toEqual('12');
+	});
 });
